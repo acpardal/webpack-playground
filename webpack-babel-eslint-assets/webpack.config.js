3a8d5d861e5dcc2432a090d32f3196acc1c01e2c
Andre Pardal <primeIT009@ctt.pt>
2016-02-03 12:33:47 +0000

    Added folder assets to better manage compiled assets

--- webpack-babel-eslint-assets/webpack.config.js
+++ webpack-babel-eslint-assets/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: path.resolve(__dirname, 'app/index.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'build', 'assets'),
+        publicPath: 'assets/',
         filename: 'bundle.js',
     },
     module: {
@@ -12,7 +13,10 @@ module.exports = {
         { test: /\.js?$/, loaders: ['babel-loader', 'eslint-loader'], exclude: /node_modules/ },
         //same as { test: /\.css$/, loader: "style-loader!css-loader" }
         { test: /\.css$/, loader: "style!css" },
-        { test: /\.(png|jpg)$/, loader: 'url?limit=10000' }
+        //images less than 10000bytes are inlined
+        { test: /\.(png|jpg)$/, loader: 'url?limit=10000' },
+        //fonts less than 10000bytes are inlined
+        { test: /\.woff$/, loader: 'url?limit=10000' }
       ]
     }
 };
